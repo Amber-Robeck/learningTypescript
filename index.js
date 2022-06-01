2a4aca448ad8b5fe3a90cb43187c6f2be04ade9e
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-05-31 23:09:11 -0500

    Updates

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 "use strict";
 //When writing a specific outDir in tcconfig, can't run the file directly on terminal or it compiles file to same folder, use tsc--watch instead
-//Start game button
-//Create table dynamically
+//Start game button--check
+//Create table dynamically--check
 //Fill table with array of words
 //Create list of words on page so user knows what to look for
 //Connect them by similar letters if length left is long enough
-//Fill in rest of empty blocks with random letter
+//Fill in rest of empty blocks with random letter--check currently filling all blocks
 //User strikethrough for words found
 //Would like to strikethrough word on list when user finds word
 //End game function
 //Hints....
+let words = ["one", "two", "three", "thirteen", "seventeen"];
 window.onload = function () {
+    createDiv();
+    console.log('page is fully loaded');
+};
+const createDiv = function () {
     let gameSpace = document.createElement("div");
     gameSpace.setAttribute("class", "game");
     let hello = document.createElement("p");
@@ -22,17 +27,20 @@ window.onload = function () {
     gameSpace.appendChild(hello);
     gameSpace.appendChild(startB);
     document.body.appendChild(gameSpace);
-    console.log('page is fully loaded');
 };
-let words = ["one", "two", "three"];
 const startGame = function () {
     console.log("started game");
     createTable();
-    displayWords();
+    hideButton();
+    getWords(words);
+    fillEmpty();
+};
+const hideButton = function () {
     const startButton = document.querySelector(".start");
     startButton.setAttribute("style", "display: none;");
 };
 //variable for row/column in table
+//had to move game variable to inside function after dynamically creating as querySelector was not able to find dynamically added.
 const block = 8;
 const createTable = function () {
     const game = document.querySelector(".game");
@@ -51,7 +59,7 @@ const createTable = function () {
     game.appendChild(table);
 };
 //char code *26 returns [ assuming first number counts as one
-const displayWords = function () {
+const fillEmpty = function () {
     for (let td of document.querySelectorAll("td")) {
         if (td.textContent === "") {
             let char = Math.round(65 + Math.random() * 25);
@@ -59,3 +67,68 @@ const displayWords = function () {
         }
     }
 };
+const getWords = function (arr) {
+    console.log(arr);
+    arr.forEach(splitWord);
+};
+// function splitWord(value: string, index: number, array: string[]) {
+//     const tableD = document.querySelectorAll("td")!;
+//     console.log("value", value);
+//     if (value.length <= block) {
+//         let letters = value.split("");
+//         console.log(letters)
+//         // ["o","n","e"]
+//         for (var i = 0; i < tableD.length; i++) {
+//             console.log("for index", i)
+//             // console.log("before innerif", tableD)
+//             // 1table 
+//             if (tableD[i].innerText === "") {
+//                 for (var l = 0; l < letters.length; l++) {
+//                     tableD[i].innerText = letters[l].toUpperCase();
+//                     // console.log("before index", tableD)
+//                     // console.log("after index", tableD)
+//                     //if return index here it returns first of every letter
+//                     console.log("l", l)
+//                 };
+//                 // if return index here last of every letter
+//                 // return
+//             }
+//         }
+//     }
+//     // return index++;
+//     // console.log("index", index);
+//     // console.log("array", array);
+// }
+// What a mess :) Callback for getwords forEach
+// If current word/value is less than block variable split into letters array
+// For each letter of letters loop through table data and if empty input letter into table data
+// Need to break after condition is met or it will just produce first letter of letters for all and no other conditions will be met
+function splitWord(value, index, array) {
+    const tableD = document.querySelectorAll("td");
+    // console.log("value", value);
+    if (value.length <= block) {
+        let letters = value.split("");
+        // console.log(letters)
+        for (var l = 0; l < letters.length; l++) {
+            console.log("letters length", letters.length);
+            for (var i = 0; i < tableD.length; i++) {
+                console.log("for index", i);
+                console.log("innertext", tableD[i].innerText);
+                if (tableD[i].innerText !== "") {
+                }
+                else {
+                    console.log("l index", l);
+                    tableD[i].innerText = letters[l].toUpperCase();
+                    break;
+                }
+                ;
+            }
+            ;
+        }
+        ;
+    }
+    ;
+    // console.log("index", index);
+    // console.log("array", array);
+}
+;
